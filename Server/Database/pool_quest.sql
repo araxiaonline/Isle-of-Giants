ee6e94d8bb46ac41cef8ce7b1bc6e45c4df2a9d3
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-04-16 19:33:15 -0400

    Create pool_quest.sql

---
+++ Server/Database/pool_quest.sql
@@ -0,0 +1,4 @@
+INSERT INTO araxia_world.pool_quest (entry,pool_entry,description) VALUES
+	 (100012,83000,'Giant Dinosaur Bones'),
+	 (100013,83000,'More Dinosaur Bones'),
+	 (100014,83000,'Many More Giant Dinosaur Bones');
