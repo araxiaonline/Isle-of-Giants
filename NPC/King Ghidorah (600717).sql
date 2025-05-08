1fafffd6e735e99bfb172c3670cbbb9426d03ab1
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2025-05-08 18:23:21 -0400

    Update King Ghidorah (600717).sql

--- NPC/King Ghidorah (600717).sql
+++ NPC/King Ghidorah (600717).sql
@@ -24,7 +24,7 @@ INSERT INTO `skinning_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `Q
 
 DELETE FROM `creature` WHERE (`id1` = 600717);
 INSERT INTO `creature` (`guid`, `id1`, `id2`, `id3`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`, `CreateObject`, `Comment`) VALUES
-(9999019, 600717, 0, 0, 1405, 0, 0, 1, 1, 0, 5920.08, 833.177, 44.7594, 5.60049, 1800, 4, 0, 2448000, 0, 1, 0, 0, 0, '', NULL, 0, NULL);
+(9999019, 600717, 0, 0, 800, 0, 5013, 1, 1, 0, 5920.08, 833.177, 44.7594, 5.60049, 1800, 4, 0, 2448000, 0, 1, 0, 0, 0, '', NULL, 0, NULL);
 
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 600717;
 
